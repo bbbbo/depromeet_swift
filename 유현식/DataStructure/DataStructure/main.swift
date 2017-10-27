e2a90d1983394e386bd97b839674d1ff3de73af8
Hyun Sik Yoo <dbgustlr92@gmail.com>
2017-10-27 22:25:12 +0900

    Stack 추가 (#9)
    * chapter10,11완료. chapter12 진행중
    
    * chapter12, 13 완료
    
    * Add Stack

---
+++ 유현식/DataStructure/DataStructure/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  DataStructure
+//
+//  Created by Hyunsik Yoo on 27/10/2017.
+//  Copyright © 2017 Hyunsik Yoo. All rights reserved.
+//
+
+import Foundation
+
+var stack = Stack()
+
+stack.push(element: 1)
+stack.push(element: 2)
+stack.push(element: 3)
+
+
+print("Stack pop : \(stack.pop()!)")
+print("Stack pop : \(stack.pop()!)")
+print("Stack pop : \(stack.pop()!)")
